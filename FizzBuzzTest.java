64094c3c2b369fbc321a727d7652f47558061e20
dburtonthompson <dburtonthompson@gmail.com>
2020-06-01 10:16:16 -0400

    proceess now what we need

--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -17,5 +17,10 @@ public class FizzBuzzTest {
         // String output = fizzBuzz.processNumber(1);
         assertEquals("1", fizzBuzz.processNumber(1));
         assertEquals("2", fizzBuzz.processNumber(2));
+        assertEquals("Fizz", fizzBuzz.processNumber(3));
+        assertEquals("Buzz", fizzBuzz.processNumber(5));
+        assertEquals("Fizz", fizzBuzz.processNumber(6));
+        assertEquals("Buzz", fizzBuzz.processNumber(10));
+        assertEquals("Fizz Buzz", fizzBuzz.processNumber(15));
     }
 }
